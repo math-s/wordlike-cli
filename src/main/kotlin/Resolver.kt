b840bf99ea5279250ddb1bdbbfcc71f413823b59
math-s <matheus.andrade1996@gmail.com>
2024-10-10 15:49:29 -0300

    Remove .gradle from repository

---
+++ src/main/kotlin/Resolver.kt
@@ -0,0 +1,8 @@
+package wordlike
+
+object Resolver {
+    fun findSimilar(target: String): Array<String> {
+        logger.debug { "finding similar words..." }
+        return arrayOf("a", "b")
+    }
+}
